91b438f26e4f677d4d8686f7f2de034fcb183f84
Ric3y <truonghoangtampro@gmail.com>
2023-10-08 03:45:18 -0500

    Add simple authentication system

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+# Hardcoded credentials
+test_user = {
+    "username": "testuser",
+    "password": "password123"
+}
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username == test_user['username'] and password == test_user['password']:
+            return render_template('index.html')
+        else:
+            return "Login Failed. Please try again."
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
